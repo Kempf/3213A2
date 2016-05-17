2c4777fb235de68d1cca45a5a8e015737cec5ae0
Paul Apelt <lt.kempf@gmail.com>
2016-05-17 16:57:05 +1000

    Update lcd.c

--- src/lcd.c
+++ src/lcd.c
@@ -1,22 +1,17 @@
 /*
-
 lcd.c for all things LCD-related
-
 pinout:
-
     pc4 rs
     pc5 enable
-
     pd0 d7
     pd1 d6
     ...
     pd7 d0
-
 commands: http://goo.gl/WheZ8n
 charset: http://goo.gl/872aBz
-
 */
 
+#include <stdio.h>
 #include <avr/io.h>
 #include <util/delay.h>
 #include "lcd.h"
@@ -31,9 +26,9 @@ void lcd_init(void)
 	
 	//initialisation of LCD screen
 	lcd_send(0,0b00011100); // function set (5x7 dot format, 2 line mode, 8-bit data)
-	lcd_send(0,0b11110000); // Blink ON, underline ON, Display ON
+	lcd_send(0,0b00110000); // Blink OFF, underline OFF, Display ON
 	lcd_send(0,0b01100000); // character entry mode with increment and display shift OFF
-	lcd_send(0,0b10000000; // clear screen
+	lcd_send(0,0b10000000); // clear screen
     lcd_clear();
 }
 
@@ -60,13 +55,13 @@ void lcd_send(unsigned char rs, unsigned char data)
 // outputs test string
 void lcd_test(void)
 {
-    lcd_send(1,0b11000110);
-    lcd_send(1,0b10101100);
-    lcd_send(1,0b10110000);
-    lcd_send(1,0b00100000);
-    lcd_send(1,0b01011110);
-    lcd_send(1,0b01011111);
-    lcd_send(1,0b01011110);
+	lcd_send(1,0b01100011);
+	lcd_send(1,0b00110101);
+	lcd_send(1,0b00001101);
+	lcd_send(1,0b00000100);
+	lcd_send(1,0b01111010);
+	lcd_send(1,0b11111010);
+	lcd_send(1,0b01111010);
 }
 
 // returns character code
@@ -74,125 +69,147 @@ unsigned char lcd_lookup(char symb)
 {
     switch(symb)
     {
-        case '0': return 0b00110000;
-        case '1': return 0b00110001;
-        case '2': return 0b00110010;
-        case '3': return 0b00110011;
-        case '4': return 0b00110100;
-        case '5': return 0b00110101;
-        case '6': return 0b00110110;
-        case '7': return 0b00110111;
-        case '8': return 0b00111000;
-        case '9': return 0b00111001;
-        case '.': return 0b00101110;
-        case 'T': return 0b01010100;
-        case 'N': return 0b01001110;
-        case 'F': return 0b01000110;
-        case 'H': return 0b01001000;
-        case 'W': return 0b01010111;
-        case 'a': return 0b01100001;
-        case 'v': return 0b01110110;
-        case 'g': return 0b01100111;
-        case 'm': return 0b01101101;
-        case 'z': return 0b01111010;
-        case 's': return 0b01110011;
-        case 'e': return 0b01100101;
-        case 'c': return 0b01100011;
-        case '/': return 0b00101111;
-        case ':': return 0b00111010;
-        case ' ': return 0b00100000;
-        default: return 0b11111111;
+		case '0': return 0b00001100;
+		case '1': return 0b10001100;
+		case '2': return 0b01001100;
+		case '3': return 0b11001100;
+		case '4': return 0b00101100;
+		case '5': return 0b10101100;
+		case '6': return 0b01101100;
+		case '7': return 0b11101100;
+		case '8': return 0b00011100;
+		case '9': return 0b10011100;
+		case '.': return 0b01110100;
+		case 'T': return 0b00101010;
+		case 'N': return 0b01110010;
+		case 'F': return 0b01100010;
+		case 'H': return 0b00010010;
+		case 'W': return 0b11101010;
+		case 'a': return 0b10000110;
+		case 'v': return 0b01101110;
+		case 'g': return 0b11100110;
+		case 'm': return 0b10110110;
+		case 'z': return 0b01011110;
+		case 's': return 0b11001110;
+		case 'e': return 0b10100110;
+		case 'c': return 0b11000110;
+		case '/': return 0b11110100;
+		case ':': return 0b01011100;
+		case ' ': return 0b00000100;
+		default: return 0b11111111;
     }
 }
 
 // outputs first stat screen
-void lcd_screen1(int t, int n, int f)
+void lcd_screen(int t, int n, float f, int r, int s, int h, float w)
 {
     char buffer[5];
-    lcd_clear();
-    // output time
-    lcd_send(1,lcd_lookup('T'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%4d",t);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup('s'));
-    lcd_send(1,lcd_lookup(' '));
-    // output no. of waveforms
-    lcd_send(1,lcd_lookup('N'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5d",n);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup(' '));
-    // freq
-    lcd_send(1,lcd_lookup('F'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5.2f",f);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup('H'));
-    lcd_send(1,lcd_lookup('z'));
-}
-
-// second stat screen
-void lcd_screen2(int t, int r, int s, int h)
-{
-    char buffer[5];
-    lcd_clear();
-    // output time
-    lcd_send(1,lcd_lookup('T'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%4d",t);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup('s'));
-    lcd_send(1,lcd_lookup(' '));
-    // round
-    lcd_send(1,lcd_lookup('R'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5d",r);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup(' '));
-    // sharp
-    lcd_send(1,lcd_lookup('S'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5d",s);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup(' '));
-    // horny
-    lcd_send(1,lcd_lookup('H'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5d",h);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup(' '));
-}
-
-// outputs third stat screen
-void lcd_screen3(int t, int w)
-{
-    char buffer[5];
-    lcd_clear();
-    // output time
-    lcd_send(1,lcd_lookup('T'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%4d",t);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup('a'));
-    lcd_send(1,lcd_lookup('v'));
-    lcd_send(1,lcd_lookup('g'));
-    lcd_send(1,lcd_lookup('W'));
-    lcd_send(1,lcd_lookup(' '));
-    // width
-    lcd_send(1,lcd_lookup('F'));
-    lcd_send(1,lcd_lookup(':'));
-    sprintf(buffer,"%5.2f",f);
-    for(c = 0; c < 5; c++)
-        lcd_send(1,lcd_lookup(buffer[c]);
-    lcd_send(1,lcd_lookup('m'));
-    lcd_send(1,lcd_lookup('s'));
+	t = (int)(t/1000);
+    unsigned char screen = t % 9;
+	switch(screen)
+	{
+		case 0:
+			// output time
+			lcd_send(0,0b00000001);
+			//lcd_clear();
+			lcd_send(1,lcd_lookup('T'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",t);
+			for(int c = 0; c < 5; c++)
+			lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('s'));
+			lcd_send(1,lcd_lookup(' '));
+			// output no. of waveforms
+			lcd_send(1,lcd_lookup('N'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",n);
+			for(int c = 0; c < 5; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup(' '));
+			// freq
+			lcd_send(0,0b00000011);
+			lcd_send(1,lcd_lookup('F'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",(int)(f*100));
+			for(int c = 0; c < 3; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('.'));
+			lcd_send(1,lcd_lookup(buffer[3]));
+			lcd_send(1,lcd_lookup(buffer[4]));
+			lcd_send(1,lcd_lookup('H'));
+			lcd_send(1,lcd_lookup('z'));
+			break;
+		case 3:
+			// output time
+			lcd_send(0,0b00000001);
+			//lcd_clear();
+			lcd_send(1,lcd_lookup('T'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",t);
+			for(int c = 0; c < 5; c++)
+			lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('s'));
+			lcd_send(1,lcd_lookup(' '));
+			// round
+			lcd_send(1,lcd_lookup('R'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",r);
+			for(int c = 0; c < 5; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup(' '));
+			// sharp
+			lcd_send(0,0b00000011);
+			lcd_send(1,lcd_lookup('S'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",s);
+			for(int c = 0; c < 5; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup(' '));
+			// horny
+			lcd_send(1,lcd_lookup('H'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",h);
+			for(int c = 0; c < 5; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup(' '));
+			break;
+		case 6:
+			// output time
+			lcd_send(0,0b00000001);
+			//lcd_clear();
+			lcd_send(1,lcd_lookup('T'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",t);
+			for(int c = 0; c < 5; c++)
+			lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('s'));
+			lcd_send(1,lcd_lookup(' '));
+			// width
+			lcd_send(0,0b00000011);
+			lcd_send(1,lcd_lookup('a'));
+			lcd_send(1,lcd_lookup('v'));
+			lcd_send(1,lcd_lookup('g'));
+			lcd_send(1,lcd_lookup('W'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",(int)(w*100));
+			for(int c = 0; c < 3; c++)
+				lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('.'));
+			lcd_send(1,lcd_lookup(buffer[3]));
+			lcd_send(1,lcd_lookup(buffer[4]));
+			lcd_send(1,lcd_lookup('m'));
+			lcd_send(1,lcd_lookup('s'));
+			break;
+		default:
+			// output time
+			lcd_send(0,0b00000001);
+			lcd_send(1,lcd_lookup('T'));
+			lcd_send(1,lcd_lookup(':'));
+			sprintf(buffer,"%5d",t);
+			for(int c = 0; c < 5; c++)
+			lcd_send(1,lcd_lookup(buffer[c]));
+			lcd_send(1,lcd_lookup('s'));
+			lcd_send(1,lcd_lookup(' '));
+			break;
+	}	
 }
