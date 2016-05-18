0a8e45dee46aff861e21ae8edf823eb89f97ad56
IchBinEineKartoffel <stephen.lonergan@hotmail.com>
2016-05-18 20:13:05 +1000

    Update toDispWaves.m
    So, can detect all circles and triangles. Horns will not test correctly, due to a small* issue with the data. The 1.5million samples are actually 150k samples, but they hold for 10 seconds. This is a bit annoying to account for, so I'm not going to. The general principle holds. 
    
    Calculating the width from this is also a bit inconvenient. So, I'm gonna leave that out of the matlab code.

--- src/waves/toDispWaves.m
+++ src/waves/toDispWaves.m
@@ -14,100 +14,100 @@ clear all;
 close all;
 
 
-%Wave1 only shows curved pulses.
-a = 1024 .* csvread('wave1.txt');
-subplot(3,1,1);
-plot(a(:,1), a(:,2));
-title('wave1');
-subplot(3,1,2);
-%Differentiation Method
-td = a(2,1) - a(1,1);
-dx = a(:,1) + (td/2);
-n = size(a);
-dx(n(1)) = [];
-plot(dx, diff(a(:,2)));
-title('differentiation of wave');
-subplot(3,1,3);
-%Integration Method (POWERRRRRRRRRR)
-w1_int = zeros(n(1),1);
-for n = 501:1500001
-    for p = 0:50:500
-        q = n - p;
-        w1_int(n) = w1_int(n) + (a(q, 2)^2/(50*td));
-    end
-end
-x_int = a(:,1);
-for n = 500:-1:1
-   x_int(n) = []; 
-   w1_int(n) = [];
-end
-plot(x_int, w1_int);
-title('Integration of wave');
-
-
-%Includes all 3. Derivation of wide triangles is pretty shitty. They are
-%very difficult to notice. Zoom in on lower plot at 3.27 seconds.
-figure;
-b = 1024 .* csvread('wave2.txt');
-subplot(3,1,1);
-title('wave2');
-plot(b(:,1), b(:,2));
-subplot(3,1,2);
-td = b(2,1) - b(1,1);
-dx = b(:,1) + (td/2);
-n = size(b);
-dx(n(1)) = [];
-plot(dx, diff(b(:,2)));
-title('differentiation of wave');
-subplot(3,1,3);
-%Integration Method (POWERRRRRRRRRR)
-w2_int = zeros(n(1),1);
-for n = 501:1500001
-    for p = 0:50:500
-        q = n - p;
-        w2_int(n) = w2_int(n) + (b(q, 2)^2/(50*td));
-    end
-end
-x_int = b(:,1);
-for n = 500:-1:1
-   x_int(n) = []; 
-   w2_int(n) = [];
-end
-plot(x_int, w2_int);
-title('Integration of wave');
-
-
-
-
-
-figure;
-c = 1024 .* csvread('wave3.txt');
-subplot(3,1,1);
-plot(c(:,1), c(:,2));
-title('wave3');
-subplot(3,1,2);
-td = c(2,1) - c(1,1);
-dx = c(:,1) + (td/2);
-n = size(c);
-dx(n(1)) = [];
-plot(dx, diff(c(:,2)));
-title('differentiation of wave');
-subplot(3,1,3);
-%Integration Method (POWERRRRRRRRRR)
-w3_int = zeros(n(1),1);
-for n = 501:1500001
-    for p = 0:50:500
-        q = n - p;
-        w3_int(n) = w3_int(n) + (c(q, 2)^2/(50*td));
-    end
-end
-x_int = c(:,1);
-for n = 500:-1:1
-   x_int(n) = []; 
-   w3_int(n) = [];
-end
-plot(x_int, w3_int);
-title('Integration of wave');
+% %Wave1 only shows curved pulses.
+% a = 1024 .* csvread('wave1.txt');
+% subplot(3,1,1);
+% plot(a(:,1), a(:,2));
+% title('wave1');
+% subplot(3,1,2);
+% %Differentiation Method
+% td = a(2,1) - a(1,1);
+% dx = a(:,1) + (td/2);
+% n = size(a);
+% dx(n(1)) = [];
+% plot(dx, diff(a(:,2)));
+% title('differentiation of wave');
+% subplot(3,1,3);
+% %Integration Method (POWERRRRRRRRRR)
+% w1_int = zeros(n(1),1);
+% for n = 501:1500001
+%     for p = 0:50:500
+%         q = n - p;
+%         w1_int(n) = w1_int(n) + (a(q, 2)^2/(50*td));
+%     end
+% end
+% x_int = a(:,1);
+% for n = 500:-1:1
+%    x_int(n) = []; 
+%    w1_int(n) = [];
+% end
+% plot(x_int, w1_int);
+% title('Integration of wave');
+% 
+% 
+% %Includes all 3. Derivation of wide triangles is pretty shitty. They are
+% %very difficult to notice. Zoom in on lower plot at 3.27 seconds.
+% figure;
+% b = 1024 .* csvread('wave2.txt');
+% subplot(3,1,1);
+% title('wave2');
+% plot(b(:,1), b(:,2));
+% subplot(3,1,2);
+% td = b(2,1) - b(1,1);
+% dx = b(:,1) + (td/2);
+% n = size(b);
+% dx(n(1)) = [];
+% plot(dx, diff(b(:,2)));
+% title('differentiation of wave');
+% subplot(3,1,3);
+% %Integration Method (POWERRRRRRRRRR)
+% w2_int = zeros(n(1),1);
+% for n = 501:1500001
+%     for p = 0:50:500
+%         q = n - p;
+%         w2_int(n) = w2_int(n) + (b(q, 2)^2/(50*td));
+%     end
+% end
+% x_int = b(:,1);
+% for n = 500:-1:1
+%    x_int(n) = []; 
+%    w2_int(n) = [];
+% end
+% plot(x_int, w2_int);
+% title('Integration of wave');
+% 
+% 
+% 
+% 
+% 
+% figure;
+% c = 1024 .* csvread('wave3.txt');
+% subplot(3,1,1);
+% plot(c(:,1), c(:,2));
+% title('wave3');
+% subplot(3,1,2);
+% td = c(2,1) - c(1,1);
+% dx = c(:,1) + (td/2);
+% n = size(c);
+% dx(n(1)) = [];
+% plot(dx, diff(c(:,2)));
+% title('differentiation of wave');
+% subplot(3,1,3);
+% %Integration Method (POWERRRRRRRRRR)
+% w3_int = zeros(n(1),1);
+% for n = 501:1500001
+%     for p = 0:50:500
+%         q = n - p;
+%         w3_int(n) = w3_int(n) + (c(q, 2)^2/(50*td));
+%     end
+% end
+% x_int = c(:,1);
+% for n = 500:-1:1
+%    x_int(n) = []; 
+%    w3_int(n) = [];
+% end
+% plot(x_int, w3_int);
+% title('Integration of wave');
 
 
 %Differentiation does not work for high signal noise. Needs a new method.
@@ -126,16 +126,70 @@ title('differentiation of wave');
 %Integration Method (POWERRRRRRRRRR)
 subplot(3,1,3)
 w4_int = zeros(n(1),1);
-for n = 501:1500001
-    for p = 0:50:500
-        q = n - p;
-        w4_int(n) = w4_int(n) + (d(q, 2)^2/(50*td));
-    end
+th = 0;
+th_latch = 0;
+start = 0;
+finish = 0;
+circles = 0;
+triangles = 0;
+horns = 0;
+data = zeros(1,70);
+start = [];
+for n = 501:1:1500001
+    data(1) = [];
+    %disp(size(data));
+    for p = 10:10:200
+            q = n - p;
+            w4_int(n) = w4_int(n) + (d(q, 2)^2/(50*td));
+    end    
+    data(70) = w4_int(n);
+    %disp(data)
+    if (w4_int(n) > 10000000) 
+        th = 3;
+        if(th_latch < 3)
+            th_latch = 3;
+        end
+    elseif (w4_int(n) > 5000000)
+            th = 2;
+            if(th_latch < 2)
+                th_latch = 2;
+            end
+    elseif (w4_int(n) > 600000)
+            th = 1;
+            if(th_latch < 1)
+                th_latch = 1;
+                start(length(start) + 1) = n;
+            end
+            
+    elseif((data(1) < 600000) &&   (data(11) < 600000) &&  (data(21) < 600000) &&  (data(31) < 600000) &&  (data(41) < 600000) &&  (data(51) < 600000) &&  (data(61) < 600000))
+        th = 0;
+        if(th_latch == 3)
+           finish = n;
+           th_latch = 0;
+           circles = circles + 1;
+        end
+        
+        if(th_latch == 2)
+           finish = n;
+           th_latch = 0;
+           triangles = triangles + 1;
+        end
+        
+        if(th_latch == 1)
+           finish = n;
+           th_latch = 0;
+           horns = horns + 1;
+        end
+    end    
 end
+horns = horns / 2;
 x_int = d(:,1);
 for n = 500:-1:1
    x_int(n) = []; 
    w4_int(n) = [];
 end
-plot(x_int, w4_int);
+p = zeros(length(start));
+start = (start ./ 50) + 50;
+y_1mil = linspace(600000,600000,1499501);
+plot(x_int, w4_int, x_int, y_1mil, 'r--', start, p, 'k*');
 title('Integration of wave');
