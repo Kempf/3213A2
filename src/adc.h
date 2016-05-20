e6af20de56cadcae905f859f390efe4f6659ea39
Paul Apelt <lt.kempf@gmail.com>
2016-05-20 12:49:23 +1000

    *cries*

--- src/adc.h
+++ src/adc.h
@@ -3,7 +3,7 @@
 
 void adc_init(void);
 void adc_test(uint16_t *data, uint16_t *n, uint16_t *r, uint16_t *s, uint16_t *h, uint16_t *f, uint16_t *w, uint16_t *ts, uint16_t *te, uint16_t tms);
-uint16_t sum_arr(uint16_t *pa);
+uint16_t sum_arr(uint16_t a[]);
 void adc_process(uint16_t *sample, uint8_t *th_latch, uint16_t *count, uint16_t *pa, uint16_t *r, uint16_t *s, uint16_t *h, uint16_t *n, uint16_t *time, uint16_t *time_start, uint16_t *time_end, uint16_t *f, uint16_t *w, uint16_t *td);
 
 #endif
